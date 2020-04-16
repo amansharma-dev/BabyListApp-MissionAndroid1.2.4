890f237587ee970f20338c05919eb350506d1e8b
amansharma-dev <amansharma.11@hotmail.com>
2020-04-16 11:43:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/babylistapp_missionandroid124/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.babylistapp_missionandroid124;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
