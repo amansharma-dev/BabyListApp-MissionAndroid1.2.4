70325538ff08bb5bd6f71e9fe312a78833eb5dcd
amansharma-dev <amansharma.11@hotmail.com>
2020-04-17 23:52:51 +0530

    commit

--- app/src/main/java/com/example/babylistapp_missionandroid124/MainActivity.java
+++ app/src/main/java/com/example/babylistapp_missionandroid124/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.babylistapp_missionandroid124;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+
+        floatingActionButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.fab:
+                Snackbar.make(view,"Sample Text",Snackbar.LENGTH_LONG)
+                        .setAction("Action",null)
+                        .show();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         getMenuInflater().inflate(R.menu.menu_main,menu);
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            Toast.makeText(getApplicationContext(),"Setting Menu Clicked",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
