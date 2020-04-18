50cce9e88c97b7d59ae7a2616d531ee39fd0373c
amansharma-dev <amansharma.11@hotmail.com>
2020-04-19 05:28:30 +0530

    commit

--- app/src/main/java/com/example/babylistapp_missionandroid124/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/babylistapp_missionandroid124/Adapter/RecyclerViewAdapter.java
@@ -2,10 +2,13 @@ package com.example.babylistapp_missionandroid124.Adapter;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -16,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.babylistapp_missionandroid124.Data.DatabaseHandler;
 import com.example.babylistapp_missionandroid124.Model.BabyItems;
 import com.example.babylistapp_missionandroid124.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -24,13 +28,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context context;
     private List<BabyItems> babyItemsList;
-    private int position;
+
 
     //confirm dialog delete
     private AlertDialog.Builder builder;
     private AlertDialog alertDialog;
-    private LayoutInflater layoutInflater;
-
+    private int position;
     public RecyclerViewAdapter(Context context, List<BabyItems> babyItemsList) {
         this.context = context;
         this.babyItemsList = babyItemsList;
@@ -72,6 +75,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public ImageButton deleteImgBtn;
         public CardView cardView;
 
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -89,16 +94,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         @Override
         public void onClick(View view) {
+            int position;
+            position = getAdapterPosition();
+            BabyItems babyItems = babyItemsList.get(position);
             switch (view.getId()){
-
 
                 case R.id.update_imageButton:
 
+                    updateItem(babyItems);
                     break;
 
                 case R.id.delete_imageButton:
-                    position = getAdapterPosition();
-                    BabyItems  babyItems = babyItemsList.get(position);
+
                     deleteItem(babyItems.getId());
                     break;
             }
@@ -110,6 +117,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             View view = LayoutInflater.from(context).inflate(R.layout.delete_confirmation_alertdialog,null);
             Button noButton = view.findViewById(R.id.confirmNo_button);
             Button yesButton = view.findViewById(R.id.confirmYes_button);
+            cardView = view.findViewById(R.id.confirm_cardView);
+            cardView.setCardBackgroundColor(context.getResources().getColor(R.color.colorCard));
 
             builder.setView(view);
             alertDialog = builder.create();
@@ -132,9 +141,68 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     alertDialog.dismiss();
                 }
             });
-
-
         }
 
+        private void updateItem(final BabyItems newBabyItems){
+            //Todo: to populate the current details in a update dialog
+            //final BabyItems babyItems = babyItemsList.get(getAdapterPosition());
+            builder = new AlertDialog.Builder(context);
+            View view  = LayoutInflater.from(context).inflate(R.layout.popup,null);
+
+             final EditText babyItemName;
+             final EditText quantity;
+             final EditText color;
+             final EditText size;
+             Button saveBtn;
+             CardView cardView;
+             final TextView title_textView;
+
+            babyItemName = view.findViewById(R.id.babyItemName_editText);
+            quantity = view.findViewById(R.id.quantity_EditText);
+            color = view.findViewById(R.id.color_EditText);
+            size = view.findViewById(R.id.size_EditText);
+            saveBtn = view.findViewById(R.id.saveButton_Button);
+            saveBtn.setText(R.string.update);
+            cardView = view.findViewById(R.id.popup_cardView);
+            cardView.setCardBackgroundColor(context.getResources().getColor(R.color.colorCard));
+
+            title_textView = view.findViewById(R.id.titleEnterDetails_textView);
+            title_textView.setText(R.string.edit_details);
+
+            babyItemName.setText(newBabyItems.getItemName());
+            quantity.setText(String.valueOf(newBabyItems.getQuantity()));
+            color.setText(newBabyItems.getColor());
+            size.setText(String.valueOf(newBabyItems.getSize()));
+
+            builder.setView(view);
+            alertDialog = builder.create();
+            alertDialog.show();
+
+            saveBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DatabaseHandler databaseHandler = new DatabaseHandler(context);
+
+                    //update Items
+                    newBabyItems.setItemName(babyItemName.getText().toString());
+                    newBabyItems.setQuantity(Integer.parseInt(quantity.getText().toString()));
+                    newBabyItems.setColor(color.getText().toString());
+                    newBabyItems.setSize(Integer.parseInt(size.getText().toString()));
+
+                    if(!babyItemName.getText().toString().isEmpty()
+                    && !quantity.getText().toString().isEmpty()
+                    && !color.getText().toString().isEmpty()
+                    && !size.getText().toString().isEmpty()){
+                        databaseHandler.updateBabyItem(newBabyItems);
+                        notifyItemChanged(getAdapterPosition(),newBabyItems);
+                    }
+                    else{
+                        Snackbar.make(view,"Enter Details.",Snackbar.LENGTH_SHORT).setTextColor(context.getResources().getColor(R.color.colorButtonDelete)).setAction("Action",null).show();
+                    }
+
+                    alertDialog.dismiss();
+                }
+            });
+        }
     }
 }
